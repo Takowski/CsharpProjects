bee11d1043b405af5d6266dd4d14609389055105
Takowski <93830775+Takowski@users.noreply.github.com>
2024-08-21 14:39:47 +0200

    maj extension

--- p3challenge3/obj/Debug/net8.0/p3challenge3.AssemblyInfo.cs
+++ p3challenge3/obj/Debug/net8.0/p3challenge3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("p3challenge3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd5432463eb833d774c08063e8193a2d56b9c062")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2112dd35d83dc4fcc72f083c3053bd97bc0e1b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("p3challenge3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("p3challenge3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
