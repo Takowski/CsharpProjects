8c2c997e547d69f39c63ac2f18ed9c297abc00c2
Takowski <93830775+Takowski@users.noreply.github.com>
2024-08-23 23:43:33 +0200

    orderid done

---
+++ orderId/Program.cs
@@ -0,0 +1,9 @@
+﻿string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+string[] orderId=orderStream.Split(",");
+Array.Sort(orderId);
+
+foreach(string order in orderId)
+{
+if(order.Length!=4)Console.WriteLine($"{order}\t - Error");
+else Console.WriteLine(order);
+}
